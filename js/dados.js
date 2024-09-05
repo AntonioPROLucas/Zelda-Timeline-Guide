21c1b182d73a62a648ecc95cf23ee6df30a52802
AntonioPROLucas <106645196+AntonioPROLucas@users.noreply.github.com>
2024-09-05 16:28:18 -0300

    Add files via upload

---
+++ js/dados.js
@@ -0,0 +1,154 @@
+const primeirosJogosZelda = [
+    {
+      title: "The Legend of Zelda: Skyward Sword",
+      lancamento: 2011,
+      cronologia: 1,
+      timeline: "Pré-Ramificação",
+      sinopse: "Link e Zelda vivem em uma ilha flutuante chamada Skyloft. Uma catástrofe os leva a Hyrule, onde Link deve salvar Zelda e o mundo.",
+      link: "https://zelda.fandom.com/wiki/The_Legend_of_Zelda:_Skyward_Sword"
+    },
+    {
+      title: "The Legend of Zelda: The Minish Cap",
+      lancamento: 2004,
+      cronologia: 2,
+      timeline: "Pré-Ramificação",
+      sinopse: "Após Zelda ser transformada em pedra por Vaati, Link encolhe para o tamanho de um Minish para salvar Hyrule e Zelda do feiticeiro maligno.",
+      link: "https://zelda.fandom.com/wiki/The_Legend_of_Zelda:_Minish_Cap"
+    },
+    {
+      title: "The Legend of Zelda: Four Swords",
+      lancamento: 2002,
+      cronologia: 3,
+      timeline: "Pré-Ramificação",
+      sinopse: "Link se divide em quatro Links separados para derrotar o maligno Vaati novamente, em uma aventura multiplayer.",
+      link: "https://zelda.fandom.com/wiki/The_Legend_of_Zelda:_Minish_Cap"
+    },
+    {
+      title: "The Legend of Zelda: Ocarina of Time",
+      lancamento: 1998,
+      cronologia: 4,
+      timeline: "Ramificação",
+      sinopse: "Link viaja no tempo com uma Ocarina do Tempo, impedindo Ganondorf de dominar Hyrule. Seus atos criam duas linhas temporais distintas.",
+      link: "https://zelda.fandom.com/wiki/The_Legend_of_Zelda:_Ocarina_of_Time"
+    },
+    {
+      title: "The Legend of Zelda: Majora's Mask",
+      lancamento: 2000,
+      cronologia: 5,
+      timeline: "Linha do Tempo da Criança",
+      sinopse: "Em busca de uma antiga companheira, Link cai no mundo de Termina, onde deve impedir a queda da lua e salvar seus habitantes.",
+      link: "https://zelda.fandom.com/wiki/The_Legend_of_Zelda:_Majora%27s_Mask"
+    },
+    {
+        title: "The Legend: of Zelda: Twilight Princess",
+        lancamento: 2006,
+        cronologia: 6,
+        timeline: "Linha do Tempo da Criança",
+        sinopse: "Hyrule é tomada pelo crepusculo, e Link se transforma em um lobo! Tendo que salvar tanto Hyrule quanto o reino do crepusculo.",
+        link: "https://zelda.fandom.com/wiki/The_Legend_of_Zelda:_Twilight_Princess"
+      },
+      {
+        title: "The Legend of Zelda: Four Swords Adventures",
+        lancamento: 2004,
+        cronologia: 7,
+        timeline: "Linha do Tempo da Criança",
+        sinopse: "Uma aventura multiplayer onde Link e seus três clones devem derrotar o mal em uma variedade de desafios.",
+        link: "https://zelda.fandom.com/wiki/The_Legend_of_Zelda:_Four_Swords_Adventures"
+      },
+      {
+        title: "The Legend of Zelda: Wind Waker",
+        lancamento: 2002,
+        cronologia: 5,
+        timeline: "Linha do Tempo do Adulto",
+        sinopse: "Hyrule está inundada, e Link embarca em uma aventura para derrotar Ganondorf e salvar o mundo.",
+        link: "https://zelda.fandom.com/wiki/The_Legend_of_Zelda:_Wind_Waker"
+      },
+      {
+        title: "The Legend of Zelda: Phantom Hourglass",
+        lancamento: 2007,
+        cronologia: 6,
+        timeline: "Linha do Tempo do Adulto",
+        sinopse: "Link explora os mares de Hyrule em uma aventura DS, enfrentando um pirata fantasma.",
+        link: "https://zelda.fandom.com/wiki/The_Legend_of_Zelda:_Phantom_Hourglass"
+      },
+      {
+        title: "The Legend of Zelda: Spirit Tracks",
+        lancamento: 2009,
+        cronologia: 7,
+        timeline: "Linha do Tempo do Adulto",
+        sinopse: "Link e Zelda viajam por Nova Hyrule em um trem, lutando contra um espírito maligno.",
+        link: "https://zelda.fandom.com/wiki/The_Legend_of_Zelda:_Spirit_Tracks"
+      },
+      {
+        title: "The Legend of Zelda: A Link to the Past",
+        lancamento: 1991,
+        cronologia: 5,
+        timeline: "Linha do Tempo do Herói Caído",
+        sinopse: "Link viaja entre o mundo da luz e o mundo das trevas para derrotar Ganon, salvar as seis donzelas e a Princesa Zelda.",
+        link: "https://zelda.fandom.com/wiki/The_Legend_of_Zelda:_A_Link_to_the_Past"
+      },
+      {
+        title: "The Legend of Zelda: Oracle of Ages/Seasons",
+        lancamento: 2001,
+        cronologia: 6/7,
+        timeline: "Linha do Tempo do Herói Caído",
+        sinopse: "Dois jogos interconectados onde Link viaja entre o passado e o futuro para salvar Holodrum e Labrynna.",
+        link: "https://zelda.fandom.com/wiki/The_Legend_of_Zelda:_Oracle_of_Seasons" // Link para Oracle of Seasons
+      },
+      {
+        title: "The Legend of Zelda: Link's Awakening",
+        lancamento: 1993,
+        cronologia: 8,
+        timeline: "Linha do Tempo do Herói Caído",
+        sinopse: "Link acorda em uma ilha misteriosa e deve encontrar o Ovo do Peixe do Vento para escapar.",
+        link: "https://zelda.fandom.com/wiki/The_Legend_of_Zelda:_Links_Awekening"
+      },
+      {
+        title: "The Legend of Zelda: A Link Between Worlds",
+        lancamento: 2013,
+        cronologia: 9,
+        timeline: "Linha do Tempo do Herói Caído",
+        sinopse: "Link adquire a habilidade de se transformar em uma pintura para explorar Hyrule e derrotar Ganon.",
+        link: "https://zelda.fandom.com/wiki/The_Legend_of_Zelda:A_Link_Between_Worlds"
+      },
+      {
+        title: "The Legend of Zelda",
+        lancamento: 1986,
+        cronologia: 10,
+        timeline: "Linha do Tempo do Herói Caído",
+        sinopse: "O primeiro jogo da série, onde Link embarca em uma jornada para resgatar a Princesa Zelda e coletar as partes da Triforce que Zelda repartiu para derrotar Ganon.",
+        link: "https://zelda.fandom.com/wiki/The_Legend_of_Zelda"
+      },
+      {
+        title: "Zelda II: The Adventure of Link",
+        lancamento: 1987,
+        cronologia: 11,
+        timeline: "Linha do Tempo do Herói Caído",
+        sinopse: "Uma continuação do primeiro jogo, com uma jogabilidade mais focada em ação e RPG. Link deve recuperar a triforce da coragem que o rei escondeu, e usa-la para libertar a Princesa Zelda da maldição lançada por seu irmão!",
+        link: "https://zelda.fandom.com/wiki/Zelda_II:_The_Adventure_of_Link"
+      },
+      {
+        title: "The Legend of Zelda: Breath of the Wild",
+        lancamento: 2017,
+        cronologia: 17,
+        timeline: "Calamidade",
+        sinopse: "Link acorda de um sono de um século para salvar Hyrule do mal de Ganon.",
+        link: "https://zelda.fandom.com/wiki/The_Legend_of_Zelda:_Breath_of_the_Wild"
+      },
+      {
+        title: "The Legend of Zelda: Tears of the Kingdom",
+        lancamento: 2023,
+        cronologia: 18,
+        timeline: "Pós Calamidade",
+        sinopse: "Link embarca em uma nova aventura para salvar Hyrule, agora fragmentado em ilhas flutuantes. Com novas habilidades, como a capacidade de fundir objetos, ele explora um mundo rico e cheio de mistérios, enfrentando inimigos e desvendando os segredos por trás da catástrofe que assola o reino.",
+        link: "https://zelda.fandom.com/wiki/The_Legend_of_Zelda:_Tears_of_the_Kingdom"
+      },
+      {
+        title: "The Legend of Zelda: Echoes of Wisdom",
+        lancamento: 2024,
+        cronologia: "...",
+        timeline: "...",
+        sinopse: "Após salvar Zelda, Link é levado por uma fenda que assola agora Hyrule! Com o poder de replicar coisas, cabe a Zelda salvar Link e seu reino.",
+        link: "https://zelda.fandom.com/wiki/The_Legend_of_Zelda:_Echoes_of_Wisdom"
+      }
+    ];
